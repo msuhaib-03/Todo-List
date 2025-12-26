f597345c432ab8bf33e214729ba1a00ab5689a69
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2025-12-26 19:53:53 +0500

    Styling of About Page and overall effect

--- src/app/MyComponent/about/about.component.ts
+++ src/app/MyComponent/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -9,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  darkMode = false;
+  isBrowser = false;
+
+  constructor() {
+    // Restore theme on refresh
+    this.darkMode = localStorage.getItem('theme') === 'dark';
+    this.applyTheme();
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    localStorage.setItem('theme', this.darkMode ? 'dark' : 'light');
+    this.applyTheme();
+  }
+
+  private applyTheme() {
+    if (!this.isBrowser) return;
+
+    document.body.classList.toggle('dark-mode', this.darkMode);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 import { TodosComponent } from './MyComponent/todos/todos.component';
 
 @Component({
@@ -10,12 +11,42 @@ import { TodosComponent } from './MyComponent/todos/todos.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  // app.component.ts (or main layout component)
+ngOnInit() {
+  if (typeof window !== 'undefined') {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 20) {
+        document.body.classList.add('scrolled');
+      } else {
+        document.body.classList.remove('scrolled');
+      }
+    });
+  }
+}
+
   title = 'todo-list-app';
-  constructor() { 
-  //   setTimeout(() => {
-  //     this.title = 'Title Changed!';
-  //   }, 2000);
-   }
+darkMode: boolean = false;
+isBrowser = false;
+
+constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+
+    if (this.isBrowser) {
+      this.darkMode = localStorage.getItem('theme') === 'dark';
+    }
+  }
+
+ toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+
+    if (this.isBrowser) {
+      localStorage.setItem('theme', this.darkMode ? 'dark' : 'light');
+    }
+  }
+
+  private applyTheme() {
+    document.body.classList.toggle('dark-mode', this.darkMode);
+  }
 }
 
 // How this module works is here.. 
